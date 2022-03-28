fb3ab7c84464be5621adaf7a39bdcacab3470466
Gao <yuegao@njmu.edu.cn>
2022-03-28 23:02:46 +0800

    3.5 finished

--- chapter3_使用dplyr进行数据转换.R
+++ chapter3_使用dplyr进行数据转换.R
@@ -103,3 +103,134 @@ arrange(flights, desc(distance / air_time))
 ##(4) 哪个航班的飞行时间最长？哪个最短？
 arrange(flights, air_time)
 arrange(flights, desc(air_time))
+
+
+##3.4 使用 select() 选择列
+select(flights, year, month, day)
+select(flights, year:day)##year和day之间的所有列
+select(flights, -(year:day))##选择不在“year”和“day”之间的所有列（不包括“year”和“day”）
+
+rename(flights, tail_num = tailnum)##重命名变量
+
+##另一种用法是将select() 函数和everything() 辅助函数结合起来使用。当想要将几个变量移到数据框开头时，这种用法非常奏效：
+select(flights, time_hour, air_time, everything())
+
+##3.4 练习
+##(1) 从flights 数据集中选择dep_time、dep_delay、arr_time 和arr_delay，通过头脑风暴找出尽可能多的方法。
+select(flights, dep_time, dep_delay, arr_time, arr_delay)
+select(flights, any_of(c("dep_time", "dep_delay", "arr_time", "arr_delay")))
+
+##(2) 如果在select() 函数中多次计入一个变量名，那么会发生什么情况？
+select(flights, time_hour, time_hour)
+
+select(flights, carrier, everything())##轻松改变列的顺序
+
+##(3) one_of() 函数的作用是什么？为什么它结合以下向量使用时非常有用？
+vars <- c(
+  "year", "month", "day", "dep_delay", "arr_delay"
+)
+
+select(flights, one_of(vars))
+
+##(4) 以下代码的运行结果是否出乎意料？选择辅助函数处理大小写的默认方式是什么？如何改变默认方式？
+select(flights, contains("TIME"))##可忽略大小写
+select(flights, contains("TIME", ignore.case = FALSE))##区分大小写
+
+
+##3.5 使用 mutate() 添加新变量
+##mutate() 总是将新列添加在数据集的最后
+flights_swl <- select(flights,
+  year:day,
+  ends_with("delay"),
+  distance,
+  air_time
+)
+view(flights_swl)
+mutate(flights_swl,
+  gain = arr_delay - dep_delay,
+  speed = distance / air_time * 60
+)
+
+##一旦创建，新列就可以立即使用：
+mutate(flights,
+  gain = arr_delay - dep_delay,
+  hours = air_time /60,
+  gain_per_hour = gain / hours
+)
+
+##只想保留新变量，使用 tansmute() 函数
+transmute(flights,
+  gain = arr_delay - dep_delay,
+  hours = air_time / 60,
+  gain_per_hour = gain / hours
+)
+
+##模运算符 %/% 整数除法 %% 求余
+transmute(flights,
+  dep_time,
+  hour = dep_time %/% 100,
+  minute = dep_time %% 100
+)
+
+##对数函数：log()、log2() 和log10()
+
+##偏移函数 lead() 和lag() 函数可以返回一个序列的领先值和滞后值。
+x <- 1:10
+lag(x)
+lead(x)
+
+##累加和滚动聚合 R 提供了计算累加和、累加积、累加最小值和累加最大值的函数：cumsum()、cumprod()、commin() 和cummax()；dplyr 还提供了cummean() 函数以计算累加均值
+x
+cumsum(x)##前n项和
+cummean(x)##前n项均值
+
+##排秩 默认的排秩方式是，最小的值获得最前面的名次，使用desc(x) 可以让最大的值获得最前面的名次：
+y <- c(1, 2, 2, NA, 3, 4)
+z <- c(1, 3, 3, NA, 2, 4)
+min_rank(y)
+min_rank(desc(y))
+min_rank(z)
+
+row_number(y)##通用排名，并列的名次结果按先后顺序不一样，靠前出现的元素排名在前
+dense_rank(y)##通用排名，并列的名次结果一样，占用下一名次。
+percent_rank(y)##按百分比的排名
+cume_dist(y)##累计分布区间的排名
+
+##3.5.2 练习
+##(1) dep_time 和 sched_dep_time 转换成从午夜开始的分钟数
+view(flights)
+mutate(flights,
+  min_dep_time = dep_time %/% 100 * 60 + dep_time %% 100,
+  min_sched_dep_time = sched_dep_time %/% 100 * 60 + sched_dep_time %% 100
+)
+
+##(2) 比较air_time 和arr_time – dep_time
+select(flights, air_time)
+select(flights, arr_time, dep_time)
+transmute(
+  flights,
+  x_time = arr_time - dep_time
+)
+##滑行时间、陆地时间
+
+##(3) 比较dep_time、sched_dep_time 和dep_delay。你期望这3 个数值之间具有何种关系？
+
+##(4) 使用排秩函数找出10 个延误时间最长的航班。如何处理名次相同的情况？仔细阅读min_rank() 的帮助文件。
+view(head(arrange(mutate(flights, delay_rank = min_rank(dep_delay + arr_delay)), desc(delay_rank)), 10))
+
+##(5) 1:3 + 1:10
+1:3 + 1:10
+
+##(6) R提供了哪些三角函数
+cos(x)
+sin(x)
+tan(x)
+acos(x)
+asin(x)
+atan(x)
+atan2(y, x)
+cospi(x)
+sinpi(x)
+tanpi(x)
+
+
